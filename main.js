fa499d8b5a61e2987adfe7cfb82244d5d48c4dc5
barioder <barioder27@gmail.com>
2022-11-28 14:18:15 +0300

    type conversions

--- main.js
+++ main.js
@@ -24,3 +24,23 @@ console.log(person)
 console.log(person.name)
 
 console.log(person.age)
+
+// type conversions 
+console.log(2 + '5')
+console.log(true + 3)
+
+console.log(Number('25')) //this returns a number instead of a string 
+console.log(Number(true))
+console.log(Number(false))
+
+console.log(parseInt('0.55'), '------') // returns int
+
+console.log(parseFloat('5.55')) // returns a float value 
+
+console.log(String(500)) // returns a string 
+console.log((200).toString())// returns a string 
+
+
+
+
+
