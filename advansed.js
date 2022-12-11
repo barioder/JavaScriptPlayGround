18990a0330932d1329d948749cd22c767ba5c51b
barioder <barioder27@gmail.com>
2022-12-11 23:56:22 +0300

    function currying

--- advansed.js
+++ advansed.js
@@ -16,8 +16,9 @@ function outer(){
 outer()
 
 console.log('Closure in Javascript')
-// Closure JavaScript
 
+
+// Closure JavaScript
 function outer1 (){
     let counter = 0
     function inner (){
@@ -32,3 +33,32 @@ const fn = outer1()
 fn()
 fn() // note how this second function returns 2 instead of 1
 fn()
+
+// Function Currying 
+console.log('Function Currying')
+
+function sum(a,b,c){
+    return a + b + c
+}
+
+// a function that takes in a function 
+function curry(fx){
+    return function(a){
+        return function(b){
+            return function(c){
+                return fx(a, b, c)
+            }
+        }
+    }
+}
+
+const currysum = curry(sum)
+
+// method 1 to execute curried function 
+console.log(currysum(4)(2)(4))
+// method 2 to execute curried functions 
+const add1 = currysum(4)
+const add2 = add1(2)
+const add3 = add2(4)
+
+console.log(add3)
