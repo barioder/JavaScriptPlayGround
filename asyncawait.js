12e3206b25f2b59d20d792fc2a05bf411f158c74
barioder <barioder27@gmail.com>
2023-02-03 18:08:27 +0300

    Sequential Execution

--- asyncawait.js
+++ asyncawait.js
@@ -29,10 +29,42 @@ async function sprint(){
     let promise = new Promise((resolve, reject)=>{
         setTimeout(()=> resolve('Run'), 2000)
     });
-
+    // The await key word suspends the sprint function execution untill the promise is settled 
     let takeoff = await promise ; //takeoff will wait 2 seconds untill promise resolves 
     // after promise resolves, then javascript continues to execute
     console.log(takeoff);
 }
 
 sprint()
+
+// Sequential Execution 
+
+// best used when a function is dependant on the other 
+
+function saysBig(){
+    return new Promise(resolve =>{
+        setTimeout(function (){
+            resolve('Big')
+        }, 2000)
+
+    })
+}
+
+function saysMove(){
+    return new Promise( resolve => {
+        setTimeout(function (){
+            resolve('Move')
+        }, 1000)
+    })
+}
+
+async function sequentialExe(){
+    const big = await saysBig()
+    console.log(big) // logs to the console after 2 
+    // saysMove() execution depends on when saysBig() finishes execution 
+    const move = await saysMove()
+    console.log(move) // waits for 2 seconds after mad then it executes as well making it 3 seconds
+}
+
+sequentialExe()
+
